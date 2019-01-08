6ae799b49bf2dc12568fd6f42e57cfa9d0dccca5
ev1stensberg <evenstensberg@gmail.com>
2019-01-08 12:37:12 +0100

    chore: add barebone

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+require('source-map-support').install();
+require('ts-node').register({
+  compilerOptions: {
+    module: 'commonjs',
+    target: 'es2017',
+  },
+});
+
+
+// IMPLEMENT ME
+module.exports = {};
